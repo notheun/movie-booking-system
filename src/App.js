7d9fde4d6985e198df3f74402ca03bddf7a99353
notheun <brendagyx67@gmail.com>
2023-04-25 13:04:21 +0800

    Replace files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import SeatSelector from "./component/seat/SeatSelector";
+import SeatManager from "./component/seat/SeatManager";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [numSeat, setNumSeat] = useState({
+    numRows: "",
+    numCols: ""
+  });
+
+  function handleSeatManagerChange(newNumSeat) {
+    setNumSeat(newNumSeat);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SeatManager numSeat={numSeat} onChange={handleSeatManagerChange} />
+      <SeatSelector numRows={numSeat.numRows} numCols={numSeat.numCols} />
     </div>
   );
 }
 
-export default App;
+/* 
+draft for testing functions only
+
+things to do:
+useState for availability of seats (done)
+convert row number to char (done)
+separate the functions:
+cust can only select seats
+manager can change num of seats
+ */
